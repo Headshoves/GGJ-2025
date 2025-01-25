ad5a2ab5ff4d8f86a20a684bd29389698687a0a1
Headshoves <60531478+Headshoves@users.noreply.github.com>
2025-01-25 11:23:12 -0300

    Feito lógica para que não acumule a queda de gravidade com o pulo duplo

--- Assets/Recursos/Scripts/Player/PlayerController.cs
+++ Assets/Recursos/Scripts/Player/PlayerController.cs
@@ -1,109 +1,110 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
-{ [SerializeField] private float _speed = 5;
-    [SerializeField] private float _jumpForce = 200;
-    [SerializeField] private float lowGravityScale = 0.5f; // Gravidade reduzida para queda lenta
-    [SerializeField] private int maxJumps = 2; // Máximo de pulos (1 pulo normal + 1 pulo duplo)
+{ 
+    [SerializeField][BoxGroup("Movimentation")] private float _speed = 5;
+    
+    [SerializeField][BoxGroup("Jump")] private float _jumpForce = 200;
+    [SerializeField][BoxGroup("Jump")] private float lowGravityScale = 0.5f; // Gravidade reduzida para queda lenta
+    [SerializeField][BoxGroup("Jump")] private int maxJumps = 2; // Maximo de pulos (1 pulo normal + 1 pulo duplo)
+    [SerializeField][BoxGroup("Jump")] private float fallingTreshold = -5f;
 
-    private Vector2 movimentInput;
 
-    [SerializeField] private bool canDash = true;
+    [SerializeField][BoxGroup("Dash")] private bool canDash = true;
+    [SerializeField][BoxGroup("Dash")] private float dasshingCooldown = 1f;
+    
     private bool isDashing;
     private float dashPower = 24f;
     private float dashingTime = 0.2f;
-    [SerializeField] private float dasshingCooldown = 1f;
 
     [SerializeField] private TrailRenderer tr;
 
-    private Rigidbody2D _rb;
-    private PlayerInput _playerInput;
+    private Rigidbody2D rb;
+    private PlayerInput playerInput;
 
+    private Vector2 movimentInput;
+    
     public bool onGround;
-    private bool isHoldingJump; // Verifica se o jogador está segurando o botão de pulo
+    private bool isHoldingJump; // Verifica se o jogador estï¿½ segurando o botï¿½o de pulo
     private bool isOnSecondJUmp;
     private int currentJumps; // Contador de saltos restantes
 
     // Ao ativar o objeto, pega o PlayerInput dele e adiciona o callback do Jump e do move
     private void OnEnable()
     {
-        _rb = GetComponent<Rigidbody2D>();
-        _playerInput = GetComponent<PlayerInput>();
-        _playerInput.actions.FindAction("Jump").performed += Jump;
-        _playerInput.actions.FindAction("Jump").canceled += StopHoldingJump; // Quando o botão é solto
-        _playerInput.actions.FindAction("Dash").performed += Dash;
-        _playerInput.actions.FindAction("Move").started += OnMove;
-        _playerInput.actions.FindAction("Move").canceled += OnMove;
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+        playerInput.actions.FindAction("Jump").performed += Jump;
+        playerInput.actions.FindAction("Jump").canceled += StopHoldingJump; // Quando o botï¿½o ï¿½ solto
+        playerInput.actions.FindAction("Dash").performed += Dash;
+        playerInput.actions.FindAction("Move").started += OnMove;
+        playerInput.actions.FindAction("Move").canceled += OnMove;
     }
 
     // Ao desativar o objeto, remove os callbacks
     private void OnDisable()
     {
-        _playerInput.actions.FindAction("Jump").performed -= Jump;
-        _playerInput.actions.FindAction("Jump").canceled -= StopHoldingJump;
-        _playerInput.actions.FindAction("Dash").performed -= Dash;
-        _playerInput.actions.FindAction("Move").started -= OnMove;
-        _playerInput.actions.FindAction("Move").canceled -= OnMove;
+        playerInput.actions.FindAction("Jump").performed -= Jump;
+        playerInput.actions.FindAction("Jump").canceled -= StopHoldingJump;
+        playerInput.actions.FindAction("Dash").performed -= Dash;
+        playerInput.actions.FindAction("Move").started -= OnMove;
+        playerInput.actions.FindAction("Move").canceled -= OnMove;
     }
 
     // Atualiza a velocidade do player de acordo com o axis gerado pelo input
     private void FixedUpdate()
     {
-        if (isDashing)
-        {
+        if (isDashing){
             return;
         }
 
-        _rb.velocity = new Vector2(movimentInput.x * _speed, _rb.velocity.y);
+        rb.velocity = new Vector2(movimentInput.x * _speed, rb.velocity.y);
 
-        // Se o jogador não estiver no chão e estiver segurando o botão de pulo
-        if (!onGround && isHoldingJump && isOnSecondJUmp)
-        {
-            _rb.gravityScale = lowGravityScale; // Aplica gravidade reduzida para cair mais devagar
+        // Se o jogador nao estiver no chao e estiver segurando o botao de pulo
+        if (!onGround && isHoldingJump && rb.velocity.y < fallingTreshold){
+            rb.gravityScale = lowGravityScale; // Aplica gravidade reduzida para cair mais devagar
         }
-        else
-        {
-            _rb.gravityScale = 8f; // Gravidade normal quando no chão ou não segurando o botão de pulo
+        else{
+            rb.gravityScale = 8f; // Gravidade normal quando no chao ou nao segurando o botao de pulo
         }
     }
 
-    // Função para inverter a direção (flip) do personagem
+    // Funcao para inverter a direcao (flip) do personagem
     private void Flip()
     {
-        // Inverte a escala do personagem na direção X
+        // Inverte a escala do personagem na direï¿½ï¿½o X
         Vector3 theScale = transform.localScale;
-        theScale.x = Mathf.Sign(_rb.velocity.x) * Mathf.Abs(theScale.x);
+        theScale.x = Mathf.Sign(rb.velocity.x) * Mathf.Abs(theScale.x);
         transform.localScale = theScale;
     }
 
-    // Método que coleta o vector para a movimentação
+    // Metodo que coleta o vector para a movimentaï¿½ï¿½o
     public void OnMove(InputAction.CallbackContext ctx) => movimentInput = ctx.ReadValue<Vector2>();
 
-    // Método de pulo básico, aplica uma força no player para cima
+    // Metodo de pulo besico, aplica uma forca no player para cima
     private void Jump(InputAction.CallbackContext ctx)
     {
-        if (onGround)
-        {
+        if (onGround){
             isOnSecondJUmp = false;
-            // Se o jogador estiver no chão, reseta o contador de pulos
+            // Se o jogador estiver no chï¿½o, reseta o contador de pulos
             currentJumps = 1; 
-            _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
         }
-        else if (currentJumps < maxJumps)
-        {
-            // Permite o segundo pulo caso o jogador ainda tenha saltos disponíveis
+        else if (currentJumps < maxJumps){
+            // Permite o segundo pulo caso o jogador ainda tenha saltos dispoiveis
             currentJumps = maxJumps;
-            _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
             //mudar isso futuramente
             isOnSecondJUmp = true;
             // StartCoroutine(calculateSecondJump());
         }
         
-        // Marque que o jogador está segurando o botão de pulo
+        // Marque que o jogador esta segurando o botaï¿½o de pulo
         isHoldingJump = true;
     }
 
@@ -113,13 +114,13 @@ public class PlayerController : MonoBehaviour
         isOnSecondJUmp = true;
     }
 
-    // Método para quando o botão de pulo é solto
+    // Metodo para quando o botao de pulo e solto
     private void StopHoldingJump(InputAction.CallbackContext ctx)
     {
         isHoldingJump = false;
     }
 
-    // Método de dash
+    // Mï¿½todo de dash
     private void Dash(InputAction.CallbackContext ctx)
     {
         if (canDash && !isDashing)
@@ -136,16 +137,16 @@ public class PlayerController : MonoBehaviour
         // Ativar o dash - movendo o personagem rapidamente
         canDash = false;
         Vector2 dashDirection = new Vector2(Mathf.Sign(movimentInput.x) * dashPower, 0);
-        _rb.velocity = dashDirection;
-        _rb.gravityScale = 0f;
+        rb.velocity = dashDirection;
+        rb.gravityScale = 0f;
         tr.emitting = true;
 
         // Espera o tempo do dash
         yield return new WaitForSeconds(dashingTime);
 
-        // Após o dash, volta a velocidade normal
+        // Apï¿½s o dash, volta a velocidade normal
         isDashing = false;
-        _rb.gravityScale = 8f;
+        rb.gravityScale = 8f;
         tr.emitting = false;
 
         // Iniciar cooldown de dash
@@ -153,7 +154,7 @@ public class PlayerController : MonoBehaviour
         canDash = true;
     }
 
-    // Chamado no FixedUpdate, verifica a direção e chama o flip
+    // Chamado no FixedUpdate, verifica a direï¿½ï¿½o e chama o flip
     private void Update()
     {
         if (movimentInput.x != 0)
@@ -161,10 +162,10 @@ public class PlayerController : MonoBehaviour
             Flip();
         }
 
-        // Verifica o estado do chão para permitir o uso do segundo pulo
+        // Verifica o estado do chï¿½o para permitir o uso do segundo pulo
         if (onGround)
         {
-            currentJumps = 1; // Resetando o contador de saltos ao tocar o chão
+            currentJumps = 1; // Resetando o contador de saltos ao tocar o chï¿½o
         }
     }
 
